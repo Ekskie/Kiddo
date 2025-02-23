57e8fa6075b14ef8bd0d1e6d34f9f438105db97a
Ekskie <107729633+Ekskie@users.noreply.github.com>
2025-02-23 16:47:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def score():
     return jsonify({'message': 'Tracing evaluated!', 'score': user_score})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",  debug=True)
+    app.run(debug=True)
